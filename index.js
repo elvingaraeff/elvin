4b951cc0c1b412617a7472b0025eca2df70b11a9
elvingaraeff <elvin.garaeff@gmail.com>
2024-04-01 05:16:59 +0400

    efwf

--- index.js
+++ index.js
@@ -22,5 +22,9 @@ module.exports = () => {
   console.log("\x1b[31m%s\x1b[0m", "                                                             ");
   console.log("\x1b[31m%s\x1b[0m", "                                                             ");
   console.log("\x1b[31m%s\x1b[0m", "                                                             ");
-  console.log("\x1b[34m%s\x1b[0m", "                Ən xoş arzu və diləklərlə!                   ");
+  console.log("\x1b[32m%s\x1b[0m", "                Ən xoş arzu və diləklərlə!                   ");
+  console.log("\x1b[31m%s\x1b[0m", "                                                             ");
+  console.log("\x1b[31m%s\x1b[0m", "                                                             ");
+  console.log("\x1b[31m%s\x1b[0m", "                                                             ");
+  console.log("\x1b[31m%s\x1b[0m", "                                                             ");
 };                                              
